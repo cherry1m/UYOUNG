d74493482d6392ad0c84cf6478bb9e5cf5ec2afe
seongeunii <jsesky1011@gmail.com>
2025-11-24 13:34:48 +0900

    feat: 전체 앱 구조 개선 및 커스텀 바텀네비 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:uyoung/app.dart';
 import 'package:uyoung/src/viewModel/memory/memeory_view_model.dart';
-
-import 'package:uyoung/src/view/pages/memory/memory_main_page.dart';
 
 void main() {
   runApp(
     MultiProvider(
       providers: [ChangeNotifierProvider(create: (_) => MemoryViewModel())],
-      child: const UyoungApp(),
+      child: const UyoungRoot(),
     ),
   );
 }
 
-class UyoungApp extends StatelessWidget {
-  const UyoungApp({super.key});
+class UyoungRoot extends StatelessWidget {
+  const UyoungRoot({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const MemoryMainPage(),
+      home: const UyoungApp(), // 실제 앱 전체 구조
     );
   }
 }
