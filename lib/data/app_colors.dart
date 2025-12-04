acfcb6429f49337423f09fe2d82fde0a641f4aa9
seongeunii <jsesky1011@gmail.com>
2025-12-05 03:25:56 +0900

    색상 정리

--- lib/data/app_colors.dart
+++ lib/data/app_colors.dart
@@ -5,9 +5,11 @@ class AppColors {
   static Color mainBlue = Color(0xFF6EA8EB);
   static Color mainNavy = Color(0xFF262932);
   static Color mainGray = Color(0xFF464D60);
-  static Color gray_03 = Color(0xFFF0F0F0);
-
-  // Calendar
   static Color mainRed = Color(0xFFE7836A);
+  static Color white_01 = Color(0xFFFAFBFE);
+
+  static Color gray_01 = Color(0xFFF5F5F5);
+  static Color gray_03 = Color(0xFFF0F0F0);
+  static Color gray_12 = Color(0xFFA0A0A0);
   static Color gray_17 = Color(0xFF414141);
 }
