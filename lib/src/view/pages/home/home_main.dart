d33d6b51611e56c1288b1cc88c2800f22aa2ae44
cherry_1m <ini_045@naver.com>
2025-12-09 13:28:53 +0900

    home main update & memory images add

--- lib/src/view/pages/home/home_main.dart
+++ lib/src/view/pages/home/home_main.dart
@@ -5,14 +5,19 @@ class HomeMain extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.white, body: _textBox());
+    return Scaffold(backgroundColor: Colors.white, body: _fullBackground());
   }
 
-  Widget _textBox() => Container(
+  // MARK: - 홈 메인 배경 이미지
+  Widget _fullBackground() => Container(
     alignment: Alignment.center,
-    child: const Text(
-      '최근 우리가 제일',
-      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    child: Image.asset(
+      'assets/images/home_main.png',
+      width: double.infinity,
+      height: double.infinity,
+      fit: BoxFit.cover,
+
+      alignment: const Alignment(0, -0.7),
     ),
   );
 }
