7d4a38f1bb574d021e977b19f9981a7719602324
cherry_1m <ini_045@naver.com>
2026-02-19 19:43:36 +0900

    수파베이스 연결

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,16 @@ import 'package:uyoung/src/viewModel/calendar/calendar_view_model.dart';
 import 'package:uyoung/src/viewModel/memory/memeory_view_model.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Supabase 초기화 추가
+  await Supabase.initialize(
+    url: 'YOUR_PROJECT_URL',
+    anonKey: 'YOUR_PUBLIC_ANON_KEY',
+  );
 
   // 날짜 포맷 초기화
   await initializeDateFormatting('ko_KR', null);
@@ -19,7 +26,6 @@ void main() async {
   runApp(
     MultiProvider(
       providers: [
-        // 메모리 뷰모델에 load() 적용
         ChangeNotifierProvider(create: (_) => MemoryViewModel()..load()),
         ChangeNotifierProvider(create: (_) => CalendarViewModel()),
       ],
