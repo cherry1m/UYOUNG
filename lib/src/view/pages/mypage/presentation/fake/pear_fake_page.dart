b90c7ed8e2441334b1111deefbb1814cbe1b7212
조성은 <jsesky1011@gmail.com>
2026-03-17 19:26:23 +0900

    Implement pearl charge page

--- lib/src/view/pages/mypage/presentation/fake/pear_fake_page.dart
+++ lib/src/view/pages/mypage/presentation/fake/pear_fake_page.dart
@@ -1,24 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:uyoung/src/view/pages/mypage/presentation/pearl_charge_page.dart';
 
 class PearFakePage extends StatelessWidget {
   const PearFakePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final w = MediaQuery.of(context).size.width;
-
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
-          child: Image.asset(
-            'assets/images/pear_charge.png',
-            width: w,
-            fit: BoxFit.fitWidth,
-          ),
-        ),
-      ),
-    );
+    return const PearlChargePage();
   }
 }
